b167c38fe5b97ad2251ac2f54c804520b64a6bd4
Ольга Малышева <olga.maysheva.smm@gmail.com>
2023-02-18 15:13:36 +0200

    done

--- learn_english/src/App.js
+++ learn_english/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Vocabulare from "./components/Vocabulare/Vocabulare";
+import Game from "./components/Game/Game";
 import "./components/Header/Header.css";
 import "./components/Footer/Footer.css";
 import "./components/Vocabulare/Vocabulare.css";
@@ -12,6 +13,7 @@ function App() {
     <div className="App">
       <Header/>
       <Vocabulare/>
+      <Game/>
       <Footer/>
     </div>
   );
