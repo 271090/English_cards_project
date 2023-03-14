82141132a08bcc81ae8a0cf9ef90a34d9dea9358
Ольга Малышева <olga.maysheva.smm@gmail.com>
2023-03-14 13:26:46 +0200

    12

--- learn_english/src/components/LayoutProject/LayoutProject.jsx
+++ learn_english/src/components/LayoutProject/LayoutProject.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 
-import Header from "../header/header";
-import Footer from "../footer/footer";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 import "./components/Header/Header.css";
 import "./components/Footer/Footer.css";
 
