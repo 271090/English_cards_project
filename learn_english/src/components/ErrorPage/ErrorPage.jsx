75cd86f0be824669e2072a675a8c43c08e766335
Ольга Малышева <olga.maysheva.smm@gmail.com>
2023-04-02 14:39:08 +0300

    image

--- learn_english/src/components/ErrorPage/ErrorPage.jsx
+++ learn_english/src/components/ErrorPage/ErrorPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../ErrorPage/ErrorPage.css";
-import Gif from "./error.gif";
+import Gif from "../images/error.gif";
 
 export default function Error() {
   return (
